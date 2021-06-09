0c96e3c3cb216024b40ad5fef82023e21ffd0e46
Karlis J <51923303+eziitis@users.noreply.github.com>
2021-06-09 17:35:48 +0300

    2nd
    2nd commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Please, let this work!
                 </div>
 
                 <div class="links">
